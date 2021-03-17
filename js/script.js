01cd312bb22989e555477983b5ce06f66a3d7409
kccnma <gargiulo@hawaii.edu>
2021-03-16 15:12:39 -1000

    updated sticky css and js w 2021 techniques

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // TOGGLE NAV 
-    var mymenubutton = document.querySelector('.menu-button');
-    var mytogglenav = document.querySelector('.toggle-nav');
-
-    mymenubutton.addEventListener('click', function () {
-        mytogglenav.classList.toggle('expanded');
+    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function () {
+        if (menunav.getAttribute('data-navstate') === 'open') {
+            // if true do this
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            // else (if false) do this:
+            menunav.setAttribute('data-navstate', 'open');
+        };
     });
 
     //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
-    var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    const stickynavlinks = document.querySelectorAll(".sticky nav a");
     for (var i = 0; i < stickynavlinks.length; i++) {
-        stickynavlinks[i].onclick = function () {
-            mytogglenav.classList.remove("expanded");
-        }
+        stickynavlinks[i].addEventListener('click', function () {
+            menunav.setAttribute('data-navstate', 'closed');
+        });
     };
 
 });
